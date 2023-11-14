7f64f83dc82cabe478bb2fb9f225a8e8361a2cc9
KonstiDoll <konstantin.doll@urban-codesign.com>
2023-11-14 12:56:48 +0100

    install typescript and setup webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devServer: {
+        client: {
+            logging: 'verbose',
+          },
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+};
